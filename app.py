9d372b0b23ba1c2b1a00fdc46b05aec7d85e9b83
Soeltan Bima Manggala Sidi <soeltan20ti@mahasiswa.pcr.ac.id>
2023-01-25 15:47:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 import os
 
